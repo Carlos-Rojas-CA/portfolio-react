9b3b0a044e988215555c68b702347ed9d5752b92
Carlos Rojas <c2rojas75@gmail.com>
2020-03-31 02:22:17 -0700

    take three

---
+++ build/precache-manifest.bb69388458c0a6306d6f96be9b49e113.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff8fa5a40e107bac2789501a84fbda2",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "99bf2def94343275e53c",
+    "url": "/portfolio-react/static/css/main.d259361c.chunk.css"
+  },
+  {
+    "revision": "bdb0d3676f38702f3c3d",
+    "url": "/portfolio-react/static/js/2.72b6bcc4.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/portfolio-react/static/js/2.72b6bcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bf2def94343275e53c",
+    "url": "/portfolio-react/static/js/main.1c5269f9.chunk.js"
+  },
+  {
+    "revision": "3d0913a0c6c6a392525e",
+    "url": "/portfolio-react/static/js/runtime-main.de05f41b.js"
+  },
+  {
+    "revision": "999e37f4aa17781466c68bc6f7e21870",
+    "url": "/portfolio-react/static/media/Carlos_Rojas Resume_t.999e37f4.pdf"
+  },
+  {
+    "revision": "83638d0a9bfd98dd96b3cdf39545feeb",
+    "url": "/portfolio-react/static/media/DSR.83638d0a.PNG"
+  },
+  {
+    "revision": "53b80021871f9233b1f2b69f1f93809e",
+    "url": "/portfolio-react/static/media/Etheron.53b80021.PNG"
+  },
+  {
+    "revision": "e241c6d6e83c39f935c7743cdedfef56",
+    "url": "/portfolio-react/static/media/MMM.e241c6d6.png"
+  },
+  {
+    "revision": "56982b0b9c689282897caea6b054f5dd",
+    "url": "/portfolio-react/static/media/Profile_Picture.56982b0b.png"
+  },
+  {
+    "revision": "2afaf81ac14b94f76f48411a05673e99",
+    "url": "/portfolio-react/static/media/connectwork.2afaf81a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
