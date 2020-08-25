f1479c945d4c7a0bfcc8afa6ba4a428461146406
Carlos Rojas <c2rojas75@gmail.com>
2020-08-24 22:14:21 -0700

    fix grammar and spelling

---
+++ build/precache-manifest.a876c84ceebeb62e4cb6c5f5353b4c6a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7e7f3ed04773137f5738c6333865fe",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "970a76fe622ed4343cfa",
+    "url": "/portfolio-react/static/css/main.d259361c.chunk.css"
+  },
+  {
+    "revision": "8010a0a3a223ee020bd8",
+    "url": "/portfolio-react/static/js/2.5c36a636.chunk.js"
+  },
+  {
+    "revision": "7d9fa850aa2ff6b3a58494c222b6b450",
+    "url": "/portfolio-react/static/js/2.5c36a636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970a76fe622ed4343cfa",
+    "url": "/portfolio-react/static/js/main.6df90cb7.chunk.js"
+  },
+  {
+    "revision": "e588d745cfe38ae2a662",
+    "url": "/portfolio-react/static/js/runtime-main.011dce12.js"
+  },
+  {
+    "revision": "f82626d2a51ff94917c4af288406ab7c",
+    "url": "/portfolio-react/static/media/Carlos_Rojas Resume_t.f82626d2.pdf"
+  },
+  {
+    "revision": "83638d0a9bfd98dd96b3cdf39545feeb",
+    "url": "/portfolio-react/static/media/DSR.83638d0a.PNG"
+  },
+  {
+    "revision": "53b80021871f9233b1f2b69f1f93809e",
+    "url": "/portfolio-react/static/media/Etheron.53b80021.PNG"
+  },
+  {
+    "revision": "e241c6d6e83c39f935c7743cdedfef56",
+    "url": "/portfolio-react/static/media/MMM.e241c6d6.png"
+  },
+  {
+    "revision": "56982b0b9c689282897caea6b054f5dd",
+    "url": "/portfolio-react/static/media/Profile_Picture.56982b0b.png"
+  },
+  {
+    "revision": "2c56dd04ec12d9c438a67bf0652709aa",
+    "url": "/portfolio-react/static/media/Tree-House-Club.2c56dd04.png"
+  },
+  {
+    "revision": "2afaf81ac14b94f76f48411a05673e99",
+    "url": "/portfolio-react/static/media/connectwork.2afaf81a.png"
+  }
+]);
